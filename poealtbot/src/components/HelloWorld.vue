4338adc3530a7262e1a2290dc750b9a7140e7a4d
Igor Fedotov <igor.fedotov@brickmakers.de>
2021-03-20 11:45:10 +0100

    add basic ui

--- poealtbot/src/components/HelloWorld.vue
+++ poealtbot/src/components/HelloWorld.vue
@@ -9,73 +9,35 @@
           height="200"
         />
       </v-col>
-
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br />please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank"
-            >Discord Community</a
-          >
-        </p>
+      <v-col cols="12">
+        <v-text-field
+          v-model="input"
+          placeholder="PoE Base Item"
+          @keyup.enter.native="click()"
+          solo
+        />
       </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">
-          What's next?
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            @click="click()"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
+      <v-col cols="6">
+        <v-list>
+          <template v-for="item in prefixes">
+            <v-list-item :key="item">
+              <v-list-item-content>
+                <v-list-item-title v-html="item"></v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </template>
+        </v-list>
       </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">
-          Important Links
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">
-          Ecosystem
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
+      <v-col cols="6">
+        <v-list>
+          <template v-for="item in suffixes">
+            <v-list-item :key="item">
+              <v-list-item-content>
+                <v-list-item-title v-html="item"></v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </template>
+        </v-list>
       </v-col>
     </v-row>
   </v-container>
@@ -83,86 +45,61 @@
 
 <script lang="ts">
 import Vue from "vue";
-import { 
+import {
   baseItem,
-   itemInfluenceTagData,
-    influenceTags,
-    rollableAffixes } from "../parser/affix_finder";
-import { BaseItem } from "../model/BaseItem";
-import { Influence } from "../model/Influence";
-
+  itemInfluenceTagData,
+  influenceTags,
+  rollableAffixes,
+} from "@/parser/affix_finder";
+// import { BaseItem } from "../model/BaseItem";
+import { Influence } from "@/model/Influence";
+import { affixTranslation } from "@/parser/affix_translator";
 export default Vue.extend({
   name: "HelloWorld",
 
   methods: {
-    click: () => {
-      const result = baseItem('Vaal Regalia')
-      console.log(result)
-      if (result != undefined) {
-        const infTagData = itemInfluenceTagData(result)
-        console.log(infTagData)
-        if (infTagData != undefined) {
-          const influences: Influence[] = [Influence.Crusader, Influence.Redeemer]
-          const infTags = influenceTags(influences, infTagData)
-          console.log(infTags)
-          const affixes = rollableAffixes(result.domain, result.tags.concat(infTags))
-          console.log(affixes)
+    click: function (event: KeyboardEvent) {
+      const result = baseItem(this.input);
+      console.log(result);
+      if (result !== undefined) {
+        const infTagData = itemInfluenceTagData(result);
+        console.log(infTagData);
+        if (infTagData !== undefined) {
+          const influences: Influence[] = [
+            Influence.Crusader,
+            Influence.Redeemer,
+          ];
+          const infTags = influenceTags(influences, infTagData);
+          console.log(infTags);
+          const affixes = rollableAffixes(
+            result.domain,
+            result.tags.concat(infTags)
+          );
+          const translations = affixes.map(affixTranslation);
+          const prefs: string[] = [];
+          const suffs: string[] = [];
+          affixes.forEach((v, i, a) => {
+            if (v.generation_type == "prefix") {
+              prefs.push(translations[i]);
+            } else {
+              suffs.push(translations[i]);
+            }
+          });
+          this.prefixes = prefs;
+          this.suffixes = suffs;
+          console.log(this.prefixes);
+          return;
         }
       }
-    }
+      this.prefixes = [""];
+      this.suffixes = [""];
+    },
   },
 
   data: () => ({
-    ecosystem: [
-      {
-        text: "vuetify-loader",
-        href: "https://github.com/vuetifyjs/vuetify-loader"
-      },
-      {
-        text: "github",
-        href: "https://github.com/vuetifyjs/vuetify"
-      },
-      {
-        text: "awesome-vuetify",
-        href: "https://github.com/vuetifyjs/awesome-vuetify"
-      }
-    ],
-    importantLinks: [
-      {
-        text: "Documentation",
-        href: "https://vuetifyjs.com"
-      },
-      {
-        text: "Chat",
-        href: "https://community.vuetifyjs.com"
-      },
-      {
-        text: "Made with Vuetify",
-        href: "https://madewithvuejs.com/vuetify"
-      },
-      {
-        text: "Twitter",
-        href: "https://twitter.com/vuetifyjs"
-      },
-      {
-        text: "Articles",
-        href: "https://medium.com/vuetify"
-      }
-    ],
-    whatsNext: [
-      {
-        text: "Explore components",
-        href: "https://vuetifyjs.com/components/api-explorer"
-      },
-      {
-        text: "Select a layout",
-        href: "https://vuetifyjs.com/getting-started/pre-made-layouts"
-      },
-      {
-        text: "Frequently Asked Questions",
-        href: "https://vuetifyjs.com/getting-started/frequently-asked-questions"
-      }
-    ]
-  })
+    input: "",
+    prefixes: [""],
+    suffixes: [""],
+  }),
 });
 </script>
